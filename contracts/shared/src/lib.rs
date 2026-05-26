d86734c116736df0d0427a4dc82c8669d63beaec
Victor Edeh <edehvictor715@gmail.com>
2026-05-26 18:39:17 +0100

    feat: add shared contract error enum (#408)

--- contracts/shared/src/lib.rs
+++ contracts/shared/src/lib.rs
@@ -1,7 +1,25 @@
 #![no_std]
 
+use soroban_sdk::contracterror;
+
 pub mod reentrancy_guard;
 pub mod state_machine;
 
 pub use reentrancy_guard::ReentrancyGuard;
 pub use state_machine::StateMachine;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum SharedError {
+    AlreadyInitialized = 1,
+    NotInitialized = 2,
+    Unauthorized = 3,
+    NotFound = 4,
+    InvalidAmount = 5,
+    InvalidState = 6,
+    DuplicateEntry = 7,
+    UnsupportedOperation = 8,
+    Overflow = 9,
+    Underflow = 10,
+}
